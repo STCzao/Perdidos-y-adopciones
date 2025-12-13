64a048d6bb0c45c704bd10c9f7157081e6df38b7
Mateo.Gonzaleez <kevinlobo0@gmail.com>
2025-12-13 16:58:31 -0300

    Agregamos un detalle al input de contraseña perteneciente al registro

--- src/pages/RegisterScreen/RegisterScreen.jsx
+++ src/pages/RegisterScreen/RegisterScreen.jsx
@@ -203,6 +203,7 @@ export default function RegisterScreen() {
           <div className="flex items-center w-full mt-8 bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
             <input
               type={show ? "text" : "password"}
+              name="password"
               placeholder="Contraseña"
               className="bg-transparent text-gray-500 placeholder-gray-500 outline-none text-sm w-full h-full"
               value={form.password}
