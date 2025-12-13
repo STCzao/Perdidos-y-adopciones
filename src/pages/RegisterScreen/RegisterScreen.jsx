e9741f6409670016d3a7d7b5fc8c86182ed32d02
Mateo.Gonzaleez <kevinlobo0@gmail.com>
2025-12-13 17:11:29 -0300

    Realizamos pequeños cambios en el registro

--- src/pages/RegisterScreen/RegisterScreen.jsx
+++ src/pages/RegisterScreen/RegisterScreen.jsx
@@ -200,7 +200,7 @@ export default function RegisterScreen() {
           )}
 
           {/* Password */}
-          <div className="flex items-center w-full mt-8 bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
+          <div className="flex items-center w-full mt-4 bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
             <input
               type={show ? "text" : "password"}
               name="password"
